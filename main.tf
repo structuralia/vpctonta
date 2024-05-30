34acee146e3da1b8e73f454eb1fd15fddcafaefe
sermartin79 <sermartin79@gmail.com>
2024-05-30 08:23:36 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,9 @@
+# -------------------------
+# Creación de la vpc
+# -------------------------
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+}
+
+
+
