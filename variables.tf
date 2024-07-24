71ce94b85fa6c413a4b49e3adb1f7f751839c65e
sermartin79 <sermartin79@gmail.com>
2024-07-24 09:07:02 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,5 +3,24 @@ variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
 
+# ----------------------------------------------------
+# Environment
+# ----------------------------------------------------
+variable "env" {
+    description = "Environment"
+    type    = string
+    default = "javierh"
+}
+
+# ----------------------------------------------------
+# Nombre del Poyecto
+# ----------------------------------------------------
+variable "project_name" {
+    description = "Npmbre del proyecto"
+    type    = string
+    default = "pharos-javierh"
+}
+
+
 
 
